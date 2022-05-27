71bc800334a62104fa0d577e19a134230c02cf21
Pouorix <pouorix@gmail.com>
2022-05-27 09:48:04 +0430

    Add:fragment and binding system

--- app/src/main/java/com/example/mobileproject2/MainActivity.kt
+++ app/src/main/java/com/example/mobileproject2/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.mobileproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.mobileproject2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding?=null
+    private val binding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Thread.sleep(3000)
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            ad d<HomeFragment>(R.id.fragment_container_view)
+        }
     }
 }
